7a5487216bf44398e0f9795c5ad9961bf0534d42
ImsirovicAjdin <ajdika@live.com>
2019-07-25 05:33:30 +0200

    Composing Observable operators with the pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Observable, range } from 'rxjs';
+import { map, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'my-app',
@@ -7,4 +9,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular';
+
+  source$: Observable<any> = range(0,10);
+
+  constructor() {
+    this.source$.pipe(
+      map(x => x *3),
+      filter(x => x % 2 === 0)
+    ).subscribe(x => console.log(x));
+  }
+
 }
